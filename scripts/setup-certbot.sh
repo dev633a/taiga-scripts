a5d3e2137de57c39ae70cae78792756c7b68b320
gmarton <gmarton@gmail.com>
2020-11-04 21:14:22 -0500

    Update certbot source to snap

--- scripts/setup-certbot.sh
+++ scripts/setup-certbot.sh
@@ -6,11 +6,10 @@ if [ ! -e ~/.setup/certbot ]; then
   touch ~/.setup/certbot
 
   if [ "$TAIGA_ENCRYPT" == "True" ]; then
-    sudo apt-get install software-properties-common -y
-    sudo add-apt-repository universe
-    sudo add-apt-repository ppa:certbot/certbot -y
-    sudo apt-get update
-    sudo apt-get install certbot python-certbot-nginx -y
+    sudo /usr/bin/snap install core; sudo snap refresh core
+    sudo /usr/bin/snap install --classic certbot
+    sudo ln -s /snap/bin/certbot /usr/bin/certbot
+    sudo apt-get install  python3-certbot-nginx -y
     sudo certbot --non-interactive --nginx -d $TAIGA_DOMAIN --agree-tos --email $TAIGA_SSL_EMAIL
   fi
 
